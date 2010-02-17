858813670250224466b37b6f21703ad3a2d30fed
xaicron <xaicron@gmail.com>
2010-02-17 22:42:43 +0900

    added diff logic

--- symlink.pl
+++ symlink.pl
@@ -3,10 +3,12 @@
 use strict;
 use warnings;
 use autodie;
+use opts;
 use FindBin qw/$Bin/;
 use File::Path qw/rmtree/;
 use Pod::Usage qw/pod2usage/;
-use opts;
+use IPC::Open3::Simple;
+use Term::ANSIColor qw/colored/;
 
 opts
     my $force => { isa => 'Bool' },
@@ -16,20 +18,38 @@ pod2usage 1 if $help;
 
 die '$ENV{HOME} not set' unless $ENV{HOME};
 
-chdir $ENV{HOME};
+my $home = $ENV{HOME}; # change safe
+chdir $home;
+
+my $ipc = IPC::Open3::Simple->new(out => sub { print "$_[0]\n" }, err => sub { warn "$_[0]\n" });
 
 opendir my($dh), $Bin;
 for my $file (readdir $dh) {
     next if $file =~ /^\.{1,2}$|^\.git$|^[^.]/;
-    if ($force && -e $file) {
-        unlink $file if -f _;
-        rmtree $file if -d _;
+    my $is_write = 1;
+    if (-e $file) {
+        if ($force) {
+            unlink $file if -f _;
+            rmtree $file if -d _;
+        }
+        else {
+            print colored ['yellow bold'], "want diff ? $file [y/n] : ";
+            $ipc->run('diff', '-u', "$Bin/$file", "$file") if yesno();
+            print colored ['red bold'], "override ? $file [y/n] : ";
+            $is_write = yesno()
+        }
     }
-    symlink "$Bin/$file", $file;
+    symlink "$Bin/$file", $file if $is_write;
 }
 closedir $dh;
 
 print "done.\n";
+
+sub yesno {
+    my $answer = <>;
+    chomp $answer;
+    return $answer =~ /[yY]/ ? 1 : 0;
+}
 
 __END__
 
