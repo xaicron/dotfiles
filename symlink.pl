1c43748beda54e64c719002819241a27065f2ef8
xaicron <xaicron@gmail.com>
2010-02-20 22:56:29 +0900

    diffの方向が逆だったのを修正

--- symlink.pl
+++ symlink.pl
@@ -30,7 +30,7 @@ for my $file (readdir $dh) {
     if (-e $file) {
         unless ($force) {
             print colored ['yellow bold'], "want diff ? $file [y/n] : ";
-            $ipc->run('diff', '-u', "$Bin/$file", "$file") if yesno();
+            $ipc->run('diff', '-u', "$file", "$Bin/$file") if yesno();
             print colored ['red bold'], "override ? $file [y/n] : ";
             $is_write = yesno()
         }
