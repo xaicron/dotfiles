709261c93af725b1e4ee23e7601604c663523917
xaicron <xaicron@gmail.com>
2018-05-01 20:24:53 +0900

    removed old cmd

--- bin/tmux-default-command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ `uname` = "Darwin" ]; then
-    which reattach-to-user-namespace 2>&1 > /dev/null;
-    if [ "$?" = "0" ]; then
-        reattach-to-user-namespace -l $SHELL
-    fi
-else
-    $SHELL
-fi
